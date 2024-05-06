3fac777a07bf370ae2c593818ac47642d81e5cc6
Frédéric De Smet <frederic.desmet@cegeka.com>
2024-05-06 22:12:18 +0200

    Implemented hello world app

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    frame: false,
+    fullscreen: false, //TODO: Set to true to run in full screen
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
